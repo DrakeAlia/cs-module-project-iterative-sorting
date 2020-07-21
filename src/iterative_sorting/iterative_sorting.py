144710f6c03c373853b305a2768040ca37d8f050
Drake Alia <DrakeTesla@macbook-pro.lan>
2020-07-21 09:29:47 -0700

    updated format

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,28 +1,36 @@
 # TO-DO: Complete the selection_sort() function below
 def selection_sort(arr):
-    # loop through n-1 elements
     # for i in range(0, len(arr) - 1):
     #     cur_index = i
     #     smallest_index = cur_index
-
     # loop through n-1 elements
-    for i in range(len(arr)-1): # for each element in the array, starting at the first index and finishing at the last index
-        smallest_index = i # declare that the smallest index is the current index's value ("grab the first element so we can compare later")
+    # for each element in the array, starting at the first index and finishing at the last index
+    for i in range(len(arr) - 1):
+        # declare that the smallest index is the current index's value, "grab the first element so we can compare later"
+        smallest_index = i 
         # TO-DO: find next smallest element
-        for j in range(i+1, len(arr)): # loop through the remainder of the array, starting with the next element and finishing with the last element
-            if arr[j] < arr[smallest_index]: # if the element is less than the smallest index element 
-                smallest_index = j # then make the smallest element that element
+         # loop through the remainder of the array, starting with the next element and finishing with the last element
+        for j in range(i+1, len(arr)):
+            # if the element is less than the smallest index element
+            if arr[j] < arr[smallest_index]:
+                # then make the smallest element that element
+                smallest_index = j 
         # TO-DO: swap
-        arr[i], arr[smallest_index] = arr[smallest_index], arr[i] # 1, 0 = 0, 1
+        # 1, 0 = 0, 1
+        arr[i], arr[smallest_index] = arr[smallest_index], arr[i] 
     return arr
 
 
-# TO-DO:  implement the Bubble Sort function below
+# TO-DO: Implement the Bubble Sort function below
 def bubble_sort(arr):
-    for i in range(len(arr)): # loop through the array
-        for j in range(len(arr)-1): # loop through the array, that will leave off the last index, we don't want to loop on the last index 
-            if arr[j] > arr[j+1]: # this is if the first element is bigger than the second element
-                arr[j], arr[j+1] = arr[j+1], arr[j] # swap the elements
+    # loop through the array
+    for i in range(len(arr)):
+    # loop through the array, that will leave off the last index, we don't want to loop on the last index
+        for j in range(len(arr) - 1):
+        # if the first element is bigger than the second element
+            if arr[j] > arr[j+1]:
+                # swap the elements
+                arr[j], arr[j+1] = arr[j+1], arr[j]
     return arr 
 
 '''
--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,22 +1,34 @@
 def linear_search(arr, target):
-    for i in range(len(arr)): # loop over every element, one by one
-        if arr[i] == target: # if the element is equal to the target
-            return i # then return the element
-    # target was not in the array
-    return -1 # not found 
+     # loop over every element, one by one
+    for i in range(len(arr)): 
+        # if the element is equal to the target
+        if arr[i] == target: 
+            return i
+    # target was not in the array, not found
+    return -1 
 
 # Write an iterative implementation of Binary Search
 def binary_search(arr, target):
-    low = 0 # declare a starting point
-    high = len(arr)-1 # declare an ending point
-    mid = 0 # declare a mid point for now
-    while high >= low: # loop
-        mid = (high + low) // 2 # find the mid point
-        if arr[mid] > target: # if the mid point is greater than the target
-            high = mid - 1 # ignore one side of the array
-        elif arr[mid] < target: # if the mid point is less than the target
-            low = mid + 1 # ignore one side of the array
-        else: # if the mid point is the target
-            return mid # return the mid point
-    # target was not in the array
-    return -1 # not found 
+    # declare a starting point
+    low = 0 
+    # declare an ending point
+    high = len(arr)-1 
+    # declare a mid point for now
+    mid = 0 
+    # loop
+    while high >= low: 
+        # find the mid point
+        mid = (high + low) // 2 
+        # if the mid point is greater than the target
+        if arr[mid] > target:
+            # ignore one side of the array 
+            high = mid - 1 
+            # if the mid point is less than the target
+        elif arr[mid] < target: 
+            # ignore one side of the array
+            low = mid + 1 
+            # if the mid point is the target
+        else: 
+            return mid 
+    # target was not in the array, not found
+    return -1 
